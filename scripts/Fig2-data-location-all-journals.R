f444c316b2324e603840b3f446b45a7426e5d442
EKaroune <ekaroune@googlemail.com>
2021-09-19 16:37:45 +0100

    data vis of all journal data locations

--- scripts/Fig2-data-location-all-journals.R
+++ scripts/Fig2-data-location-all-journals.R
@@ -10,6 +10,21 @@ Open_phytos <- read_csv(here("raw data", "Open_Phyto_2020_cleaned.csv"))
 Data_location_all_journal <- Open_phytos %>% 
   rename_with(make.names) %>%
   select("Journal", "Data.location") %>% 
-  group_by(Data.location) %>% 
+  group_by(Journal) %>% 
   count(Data.location)
 
+
+data_locations_all_percent <- Data_location_all_journal %>% 
+  mutate (Percent = n / sum(n) * 100)
+  
+library(ggplot2)
+
+data_locations_all_percent %>% 
+  ggplot(aes(x = Journal, y = Percent, fill = Data.location)) +
+  geom_col() +
+  scale_fill_brewer(palette="Dark2") +
+  theme_minimal() +
+  labs(title = "Location of phytolith data in journals sampled",
+       x = "Journal",
+       y = " Percentage of articles")
+  
