0cd9a09149936cf56b7bd4f7d7101b38288c933f
EKaroune <ekaroune@googlemail.com>
2021-09-25 15:02:00 +0100

    adding labels to the scripts

--- scripts/Fig2-data-location-all-journals.R
+++ scripts/Fig2-data-location-all-journals.R
@@ -5,7 +5,7 @@ library(here)
 #reading in the data and rename it as a tibble with shorter name
 Open_phytos <- read_csv(here("raw data", "Open_Phyto_2020_cleaned.csv"))
 
-# selecting the data column and rows for figure
+# selecting the data column and rows for figure, counting by data location
 
 Data_location_all_journal <- Open_phytos %>% 
   rename_with(make.names) %>%
@@ -13,21 +13,22 @@ Data_location_all_journal <- Open_phytos %>%
   group_by(Journal) %>% 
   count(Data.location)
 
-
+# calculating percentage
 data_locations_all_percent <- Data_location_all_journal %>% 
   mutate (Percent = n / sum(n) * 100)
   
 library(ggplot2)
 
+# plotting a barchart 
 data_locations_all_percent %>% 
-  ggplot(aes(x = factor(Journal), y = Percent, fill = Data.location)) +
-  geom_col(color = "black") +
-  theme_minimal() +
+  ggplot(aes(x = factor(Journal), y = Percent, fill = Data.location)) + #selecting the x and y axis values and also the stacked bar fills fpr the legend 
+  geom_col(color = "black") + # making a stacked bar chart that has black outline
+  theme_minimal() + #graph theme
   labs(title = "Location of phytolith data in journals sampled",
        x = "Journal",
-       y = " Percentage of articles") +
+       y = " Percentage of articles") + #labelling the graph
   theme(axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1, size = 8)) +
   scale_x_discrete(limits = c("Vegetation History and Archaeobotany", "Archaeological and Anthropological Sciences", "Environmental Archaeology", "The Holocene", "Journal of Archaeological Science", "Journal of Archaeological Science reports", "Quaternary International", "Antiquity", "Journal of Field Archaeology",	
 "Journal of Anthropological Archaeology", "PLoS One","Proceedings of the National Academy of Sciences")) +
-  scale_fill_brewer(palette = "Dark2", limits = c("N", "Graph", "Table", ".docx", "pdf", ".xlsx", ".csv", "Repository"))
+  scale_fill_brewer(palette = "Dark2", limits = c("N", "Graph", "Table", ".docx", "pdf", ".xlsx", ".csv", "Repository")) #Dark2 is a colour blind friendly palette 
   
