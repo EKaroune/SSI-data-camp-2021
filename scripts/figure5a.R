3d0014f5283cd2436ad62e3028caa3b7f707f7b3
EKaroune <ekaroune@googlemail.com>
2021-10-03 14:35:20 +0100

    adding script for all columns

--- scripts/figure5a.R
+++ scripts/figure5a.R
@@ -5,11 +5,39 @@ library(here)
 #reading in the data and rename it as a tibble with shorter name
 Open_phytos <- read_csv(here("raw data", "Open_Phyto_2020_cleaned.csv"))
 
-#selecting column
+#selecting column for reuse_rawdata
 reuseable_rawdata <- Open_phytos %>%
 rename_with(make.names) %>%
   select(12) %>%
   count(Reuse.data) %>% 
+  mutate(percent = n / sum(n) * 100)
+
+#picture
+pics <- Open_phytos %>%
+  rename_with(make.names) %>%
+  select(13) %>%
+  count(Phyto.pictures.present) %>% 
+  mutate(percent = n / sum(n) * 100)
+
+#Open access
+open_access <- Open_phytos %>%
+  rename_with(make.names) %>%
+  select(14) %>%
+  count(OA) %>% 
+  mutate(percent = n / sum(n) * 100)
+
+#ICPN
+icpn <- Open_phytos %>%
+  rename_with(make.names) %>%
+  select(17) %>%
+  count(Used.ICPN.1.0) %>% 
+  mutate(percent = n / sum(n) * 100)
+
+#method
+method <- Open_phytos %>%
+  rename_with(make.names) %>%
+  select(18) %>%
+  count(Full.methods.given) %>% 
   mutate(percent = n / sum(n) * 100)
 
 library(ggplot2)
